309e1e4d895d644cf428e058d17dabeb86842270
Jagoda Jerkovic <jjerkovic@dspace.hr>
2022-01-27 21:13:57 +0100

    Add project files.

---
+++ CrtanjeKruznice/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrtanjeKruznice
+{
+    public partial class Form1 : Form
+    {
+        // public bool crtaj = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        public void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+        public void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Kruznica.srediste = MousePosition;
+            Kruznica.srediste = this.PointToClient(MousePosition);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Kruznica.obodnaTocka = this.PointToClient(MousePosition);
+
+            labelPromjer.Text = "Promjer kruznice = " + Kruznica.Promjer().ToString() + " Koordinate sredista: " + Kruznica.srediste.ToString();
+            Graphics g = this.CreateGraphics();
+            Kruznica.IscrtajKruznicu(g);
+        }
+
+
+    }
+}
---
+++ CrtanjeKruznice/Kruznica.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CrtanjeKruznice
+{
+    public static class Kruznica
+    {
+
+
+        public static Point srediste;
+        public static Point obodnaTocka;
+
+        public static int Promjer()
+        {
+            double promjer =  2*Math.Sqrt(Math.Pow((srediste.X - obodnaTocka.X), 2) + Math.Pow((srediste.Y - obodnaTocka.Y), 2));
+
+            return (int)promjer;
+        }
+
+        // metoda IscrtajKruznicu
+
+        public static void IscrtajKruznicu(Graphics g)
+        {
+
+            int promjer = Kruznica.Promjer();
+
+            // stranica kvadrata = promjer kruznice
+            // koordinate kvadrata - pocetna tocka iscrtavanja je gore lijevo u odnosu na srediste kruznice
+            Rectangle kvadrat = new Rectangle((srediste.X - promjer/2), (srediste.Y - promjer / 2), promjer, promjer);
+
+            // iscrtaj kruznicu
+            g.DrawEllipse(Pens.Black, kvadrat);
+        }
+
+    }
+}
